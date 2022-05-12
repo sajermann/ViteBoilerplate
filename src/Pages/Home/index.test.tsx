38117f4b0c42c259235ad5e2b106b767f3de3ce1
Bruno Sajermann <sajermannbruno@gmail.com>
2022-05-12 09:00:00 -0300

    Adjusts tests

--- src/Pages/Home/index.test.tsx
+++ src/Pages/Home/index.test.tsx
@@ -1,14 +1,59 @@
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import Home from './index';
+import {
+  getByPlaceholderText,
+  getByTestId,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Home from "./index";
 
 function Mock() {
-	return <Home />;
+  return <Home />;
 }
 
-describe('Pages/Users/UserAdd', () => {
-	test(`Must contain the Batata`, () => {
-		render(<Mock />);
-		expect(screen.getByTestId("Batata")).toBeInTheDocument();
-	});
+describe("Pages/Home", () => {
+  it(`should render list items`, () => {
+    const { getByText } = render(<Mock />);
+    expect(getByText("Bruno")).toBeInTheDocument();
+    expect(getByText("Marcia")).toBeInTheDocument();
+  });
+
+  it(`should add new item to the list`, async () => {
+    const { getByText, debug, getByTestId, findByText } = render(<Mock />);
+    const inputElement = getByTestId("inputNewItem");
+    const addButton = getByText("Adicionar");
+    debug();
+    await userEvent.type(inputElement, "Dereck");
+    await userEvent.click(addButton);
+    debug();
+
+    // Modo 1
+    expect(await findByText("Dereck")).toBeInTheDocument();
+
+    //Modo 2
+    await waitFor(() => {
+      expect(getByText("Dereck")).toBeInTheDocument();
+    });
+  });
+
+  it(`should remove item to the list`, async () => {
+    const { getByText, getAllByText, debug, getByTestId, queryByText } = render(
+      <Mock />
+    );
+    const removeButton = getAllByText("Remover");
+    debug();
+    await userEvent.click(removeButton[0]);
+    debug();
+
+    // Modo 1
+    await waitFor(() => {
+      expect(queryByText("Bruno")).not.toBeInTheDocument();
+    });
+    //Modo 2
+    // await waitForElementToBeRemoved(() => {
+    //   return getByText("Bruno");
+    // });
+  });
 });
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,5 +1,44 @@
-export default function Home(){
-  return(
-    <div data-testid="Batata">Home</div>
-  )
+import { useState } from "react";
+
+export default function Home() {
+  const [newItem, setNewItem] = useState("");
+  const [list, setList] = useState(["Bruno", "Marcia"]);
+
+  function addToList() {
+    setTimeout(() => {
+      setList((state) => [...state, newItem]);
+    }, 500);
+    
+  }
+
+  function removeFromList(itemRemove:string) {
+    setTimeout(() => {
+      setList((state) => state.filter(item=>item !== itemRemove));
+    }, 500);
+    
+  }
+  return (
+    <div data-testid="Batata">
+      <input
+        type="text"
+        data-testid="inputNewItem"
+        placeholder="Novo Item"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button type="button" onClick={addToList}>
+        Adicionar
+      </button>
+      <ul>
+        {list.map((item) => (
+          <li key={item}>
+            {item}
+           <button type="button" onClick={()=>removeFromList(item)}>
+        Remover
+      </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
