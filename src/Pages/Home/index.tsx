eec958dcd67de71aac96ecbb91e5f0e26b49ca76
Bruno Sajermann <sajermannbruno@gmail.com>
2022-05-13 08:19:35 -0300

    add tailwind

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,44 +1,42 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function Home() {
-  const [newItem, setNewItem] = useState("");
-  const [list, setList] = useState(["Bruno", "Marcia"]);
+	const [newItem, setNewItem] = useState('');
+	const [list, setList] = useState(['Bruno', 'Marcia']);
 
-  function addToList() {
-    setTimeout(() => {
-      setList((state) => [...state, newItem]);
-    }, 500);
-    
-  }
+	function addToList() {
+		setTimeout(() => {
+			setList(state => [...state, newItem]);
+		}, 500);
+	}
 
-  function removeFromList(itemRemove:string) {
-    setTimeout(() => {
-      setList((state) => state.filter(item=>item !== itemRemove));
-    }, 500);
-    
-  }
-  return (
-    <div data-testid="Batata">
-      <input
-        type="text"
-        data-testid="inputNewItem"
-        placeholder="Novo Item"
-        value={newItem}
-        onChange={(e) => setNewItem(e.target.value)}
-      />
-      <button type="button" onClick={addToList}>
-        Adicionar
-      </button>
-      <ul>
-        {list.map((item) => (
-          <li key={item}>
-            {item}
-           <button type="button" onClick={()=>removeFromList(item)}>
-        Remover
-      </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	function removeFromList(itemRemove: string) {
+		setTimeout(() => {
+			setList(state => state.filter(item => item !== itemRemove));
+		}, 500);
+	}
+	return (
+		<div data-testid="Batata">
+			<input
+				type="text"
+				data-testid="inputNewItem"
+				placeholder="Novo Item"
+				value={newItem}
+				onChange={e => setNewItem(e.target.value)}
+			/>
+			<button type="button" onClick={addToList} className="bg-brand-500">
+				Adicionar
+			</button>
+			<ul>
+				{list.map(item => (
+					<li key={item}>
+						{item}
+						<button type="button" onClick={() => removeFromList(item)}>
+							Remover
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
