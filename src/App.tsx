82fe3e56ef61ad86b6c02e2e23bff356c45d29a3
Bruno Sajermann <sajermannbruno@gmail.com>
2022-05-12 22:49:06 -0300

    Adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import Home from './Pages/Home'
-import './App.css'
+import { useState } from 'react';
+import logo from './logo.svg';
+import Home from './Pages/Home';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-     <Home />
-    </div>
-  )
+	return (
+		<div className="App">
+				<Home />
+		</div>
+	);
 }
 
-export default App
+export default App;
