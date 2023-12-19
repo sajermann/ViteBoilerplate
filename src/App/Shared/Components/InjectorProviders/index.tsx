e3b8a9e73247054d8bbf47c9efecfd54f02b73c3
Bruno Sajermann <sajermannbruno@gmail.com>
2023-12-18 21:22:46 -0300

    fix

--- src/App/Shared/Components/InjectorProviders/index.tsx
+++ src/App/Shared/Components/InjectorProviders/index.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 import { Header } from '../Header';
 import { DarkModeProvider } from '../../Hooks/UseDarkMode';
 import { VerifyAuth } from '../VerifyAuth';
