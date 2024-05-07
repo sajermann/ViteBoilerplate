ed63c2fb3d82c4d5daf5aad117c0331658adf816
Sajermann <sajermannbruno@gmail.com>
2024-05-07 20:51:21 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		port: 5000,
+	},
+	preview: {
+		port: 8080,
+	},
 	plugins: [react()],
 	resolve: {
 		alias: {
