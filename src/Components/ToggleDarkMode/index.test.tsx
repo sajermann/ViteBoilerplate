0fdff9c3d8b0adb31e3c0c9386736746a011150c
Sajermann <sajermannbruno@gmail.com>
2025-01-07 08:48:04 -0300

    change eslint and prettier to biomejs

--- src/Components/ToggleDarkMode/index.test.tsx
+++ src/Components/ToggleDarkMode/index.test.tsx
@@ -2,44 +2,43 @@
  * @vitest-environment jsdom
  */
 import { fireEvent, render } from '@testing-library/react';
-import { it, describe, expect, vi, SpyInstance } from 'vitest';
+import { MockInstance, describe, expect, it, vi } from 'vitest';
 import { ToggleDarkMode } from '.';
 import '../../Config/i18n';
 import { DarkModeProvider, useDarkMode } from '../../Hooks/UseDarkMode';
 
 function Mock0() {
-	const { darkMode } = useDarkMode();
-	return (
-		<>
-			<ToggleDarkMode />
-			<div>{darkMode ? <span>DarkModeOn</span> : <span>DarkModeOff</span>}</div>
-		</>
-	);
+  const { darkMode } = useDarkMode();
+  return (
+    <>
+      <ToggleDarkMode />
+      <div>{darkMode ? <span>DarkModeOn</span> : <span>DarkModeOff</span>}</div>
+    </>
+  );
 }
 
 function Mock1() {
-	return (
-		<DarkModeProvider>
-			<Mock0 />
-		</DarkModeProvider>
-	);
+  return (
+    <DarkModeProvider>
+      <Mock0 />
+    </DarkModeProvider>
+  );
 }
 
 describe('Components/ToggleDarkMode', () => {
-	it(`should change dark mode`, async () => {
-		(
-			vi.spyOn(Storage.prototype, 'getItem') as SpyInstance<
-				Array<string>,
-				string | null
-			>
-		).mockReturnValue('true');
-		const { getByText, debug } = await render(<Mock1 />);
-		await fireEvent.click(await getByText('🌞'));
-		expect(await getByText('DarkModeOff')).toBeInTheDocument();
-		debug();
+  it(`should change dark mode`, async () => {
+    (
+      vi.spyOn(Storage.prototype, 'getItem') as MockInstance<
+        (key: string) => string | null
+      >
+    ).mockReturnValue('true');
+    const { getByText, debug } = await render(<Mock1 />);
+    await fireEvent.click(await getByText('🌞'));
+    expect(await getByText('DarkModeOff')).toBeInTheDocument();
+    debug();
 
-		await fireEvent.click(await getByText('🌜'));
-		expect(await getByText('DarkModeOn')).toBeInTheDocument();
-		debug();
-	});
+    await fireEvent.click(await getByText('🌜'));
+    expect(await getByText('DarkModeOn')).toBeInTheDocument();
+    debug();
+  });
 });
