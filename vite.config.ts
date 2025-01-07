dc9d368e41dab672219b9d2aa4dd4164c1b8bf04
Sajermann <sajermannbruno@gmail.com>
2025-01-07 08:48:16 -0300

    update vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-/* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import * as path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	server: {
-		port: 5000,
-	},
-	preview: {
-		port: 8080,
-	},
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'~': path.resolve(__dirname, 'src'),
-		},
-	},
+  server: {
+    port: 5000,
+  },
+  preview: {
+    port: 8080,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,19 @@
 /// <reference types="vitest" />
-/* eslint-disable import/no-extraneous-dependencies */
 import * as path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'~': path.resolve(__dirname, 'src'),
-		},
-	},
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './src/Config/Test/setup.ts',
-		coverage: {
-			reporter: ['text', 'lcov', 'html'],
-		},
-	},
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/Config/Test/setup.ts',
+    coverage: {
+      reporter: ['text', 'lcov', 'html'],
+    },
+  },
 });
