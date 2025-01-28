56ddede996557f21398eba3dc0e4687f3147b05a
Sajermann <sajermannbruno@gmail.com>
2025-01-28 19:45:19 -0300

    adjusts

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,9 +11,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/Config/Test/setup.ts',
+    setupFiles: './src/config/test/setup.ts',
     coverage: {
       reporter: ['text', 'lcov', 'html'],
+      exclude: ['**/index.tsx', '**/*.d.ts', '**/types/**', '**/interface/**'],
+      include: ['src/**'],
     },
   },
 });
